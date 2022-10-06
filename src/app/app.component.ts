7b24d2362e70c3ad9d294340c3f3edd410c3a812
Nadeem Sarwar <na.sarwar@gemsol.in>
2022-10-06 23:44:42 +0530

    Assignment-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'assignment1';
+  title = "assignment1";
+  isClicked: boolean = false;
+  count: number = 0;
+  id: number = 0;
+  texts: string[] = [];
+  onclick() {
+    this.count++;
+    this.isClicked = !this.isClicked;
+  }
+  OnSubmit(data) {
+    if (data == "") {
+      alert("Please enter data");
+    }else{
+      this.id++;
+      this.texts.push(data);
+    }
+    
+    
+  }
 }
